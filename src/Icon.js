e5f1331e6ae59b4281dec860bbd3dbecadefaa56
Norbert Renner <norbert.renner@gmx.de>
2019-12-18 19:52:46 +0100

    Prefix default shadowUrl with 'dist/'

--- src/Icon.js
+++ src/Icon.js
@@ -2,7 +2,7 @@ import Leaflet from 'leaflet'
 
 const iconOptions = {
 
-  shadowUrl: 'images/marker-shadow.png',
+  shadowUrl: 'dist/images/marker-shadow.png',
   iconSize:    [25, 41],
   shadowSize:  [41, 41],
   iconAnchor:  [12, 41],
